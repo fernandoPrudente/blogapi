80dc351ce36c4f583698d06178b712c10752ce02
fernandoPrudente <41087156+fernandoPrudente@users.noreply.github.com>
2018-11-16 20:27:41 -0200

    Update deploytest.sh

--- deploy/deploytest.sh
+++ deploy/deploytest.sh
@@ -1,4 +1,4 @@
-docker run --rm fpmatos/blogapi1 echo "teste de container OK"
+docker run --rm fpmatos/blogapi echo "teste de container OK"
 
 if [ ! $? -eq 0 ]; then
  echo "Erro durante o teste de deploy" >> deploy_log.txt 
